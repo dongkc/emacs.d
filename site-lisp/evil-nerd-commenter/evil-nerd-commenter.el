f5fa85569b6921c2ddb782567fd60cba2efa198c
dongkc <dongkechang@gmail.com>
2017-04-05 16:32:16 +0800

    add org-mode.el from http://doc.norang.ca/org-mode.html

--- site-lisp/evil-nerd-commenter/evil-nerd-commenter.el
+++ site-lisp/evil-nerd-commenter/evil-nerd-commenter.el
@@ -694,10 +694,10 @@ If NO-EVIL-KEYBINDINGS is t, we don't define keybindings in evil-mode."
   (interactive)
 
   ;; Install hotkeys for Emacs mode
-  (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
-  (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
-  (global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
-  (global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
+  ;;(global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+  ;;(global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
+  ;;(global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
+  ;;(global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
 
   ;; Install key bindings for evil
   (unless no-evil-keybindings
