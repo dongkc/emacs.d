7d18d7fa82d8b132b19dcf643c5b51faa265a835
dongkechang <dongkechang@nuctech.com>
2020-05-21 10:03:45 +0800

    add omnisharp support for windows platform

--- custom.el
+++ custom.el
@@ -36,5 +36,33 @@
 (auth-source-pass-enable)
 
 (load-theme 'sanityinc-tomorrow-eighties t)
- (setq exec-path (append exec-path '("/Users/dongkc/flutter/bin/")))
+
+(setq exec-path (append exec-path '("/Users/dongkc/flutter/bin/")))
 (toggle-frame-fullscreen)
+
+(eval-after-load
+  'company
+  '(add-to-list 'company-backends #'company-omnisharp))
+
+(defun my-csharp-mode-setup ()
+  (omnisharp-mode)
+  (company-mode)
+  (flycheck-mode)
+
+  (setq indent-tabs-mode nil)
+  (setq c-syntactic-indentation t)
+  (c-set-style "ellemtel")
+  (setq c-basic-offset 4)
+  (setq truncate-lines t)
+  (setq tab-width 4)
+  (setq evil-shift-width 4)
+
+  ;csharp-mode README.md recommends this too
+  ;(electric-pair-mode 1)       ;; Emacs 24
+  ;(electric-pair-local-mode 1) ;; Emacs 25
+
+  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
+  (local-set-key (kbd "C-c C-c") 'recompile))
+
+(add-hook 'csharp-mode-hook 'my-csharp-mode-setup t)
+(setq omnisharp-expected-server-version "1.35.1")
