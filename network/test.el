50b9be6c346c55a2cad7a2b2457110d8e8d119c9
dongkechang <dongkechang@nuctech.com>
2020-06-04 09:50:14 +0800

    remove garbage debug code

--- network/test.el
+++ network/test.el
@@ -12,18 +12,6 @@
   (message (binary-to-hex message))
     (tcp-server-trace proc (concat "R:" (binary-to-hex message))))
 
-(setq binary-data
-      [ 192 168 1 100 -192 168 1 101 01 28 21 32
-            160 134 1 0 5 1 0 0 2 0 0 0])
-
-(defun binary-to-hex2(binary)
-  "convert binary array to hex string"
-  (interactive)
-          (seq-mapn (lambda (elt)
-                      ;; (format "%02X" (abs elt) )) binary-data) " ")
-                      (format "%02X" (abs elt) )) binary)
-  )
-
 
 (defun binary-to-hex(binary)
   "convert binary array to hex string"
@@ -34,6 +22,6 @@
                       (format "%02X" (logand 255 elt) )) binary) " ")
   )
 
-(message (binary-to-hex binary-data))
+;; (message (binary-to-hex binary-data))
 
-(message (format "%x" binary-data))
+;; (message (format "%x" binary-data))
